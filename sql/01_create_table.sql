d86dfa3ff774621fdaea3f94611a13c4017131f5
sonalagrawal1403-sys <sonalagrawal1403@gmail.com>
2025-12-03 17:01:29 +0530

    Create 01_create_table.sql

---
+++ sql/01_create_table.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS superstore_ascii_only;
+
+CREATE TABLE superstore_ascii_only (
+    Row_ID INT,
+    Order_ID VARCHAR(50),
+    Order_Date DATE,
+    Ship_Date DATE,
+    Ship_Mode VARCHAR(50),
+    Customer_ID VARCHAR(50),
+    Customer_Name VARCHAR(100),
+    Segment VARCHAR(50),
+    Country VARCHAR(50),
+    City VARCHAR(100),
+    State VARCHAR(100),
+    Postal_Code VARCHAR(20),
+    Region VARCHAR(50),
+    Product_ID VARCHAR(50),
+    Category VARCHAR(50),
+    Sub_Category VARCHAR(50),
+    Product_Name VARCHAR(255),
+    Sales DECIMAL(10,2),
+    Quantity INT,
+    Discount DECIMAL(5,2),
+    Profit DECIMAL(10,2)
+);
+USE superstore_ascii_only;
